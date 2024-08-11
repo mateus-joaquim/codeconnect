eed4796eb6b0086a17e75488396ea96834ad3b90
mateus-joaquim <m.joaquim.fs@gmail.com>
2024-08-11 15:44:08 -0300

    Adicionado: Fonte e variaveis de cores + estilizacao Sidebar

--- src/componentes/Sidebar/index.jsx
+++ src/componentes/Sidebar/index.jsx
@@ -3,36 +3,38 @@ import Feed from "./assets/feed.svg";
 import Perfil from "./assets/account_circle.svg";
 import Sobre from "./assets/info.svg";
 import Logout from "./assets/logout.svg";
-
+import "./styles.css";
 export default function Sidebar() {
   return (
     <aside>
       <img src={Logo} alt="Logo do CodeConnect" />
       <nav>
-        <ul>
+        <ul className="lista-sidebar">
           <li>
-            <a href="#">Publicar:</a>
+            <a href="#" className="item__link-publicacao">
+              Publicar
+            </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="item__link item__link--ativo">
               <img src={Feed} alt="" />
               <span>Feed</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="item__link">
               <img src={Perfil} alt="" />
               <span>Perfil</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="item__link">
               <img src={Sobre} alt="" />
               <span>Sobre nós</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="item__link">
               <img src={Logout} alt="" />
               <span>Sair</span>
             </a>
