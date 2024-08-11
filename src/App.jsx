ac168b32da2b2ed61d5f6817be93e5c68e6aecb6
mateus-joaquim <m.joaquim.fs@gmail.com>
2024-08-11 17:45:05 -0300

    Adicionado: componente Ordenacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import BarraDePesquisa from "./componentes/BarraDePesquisa";
 import Filtro from "./componentes/Filtro";
+import Ordenacao from "./componentes/Ordenacao";
 import Sidebar from "./componentes/Sidebar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div>
         <BarraDePesquisa />
         <Filtro />
+        <Ordenacao />
       </div>
     </div>
   );
---
+++ src/componentes/Ordenacao/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Ordenacao() {
+  return (
+    <section className="container-ordenacao">
+      <ul>
+        <li className="ordenacao_item-lista ordenacao-ativo">Recentes</li>
+        <li className="ordenacao_item-lista">Recentes</li>
+      </ul>
+    </section>
+  );
+}
