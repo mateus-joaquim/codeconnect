1fadb62146ef991475c90685635b8f2e8125d909
mateus-joaquim <m.joaquim.fs@gmail.com>
2024-08-12 16:09:07 -0300

    Adicionado: Requisicao das infos dos Cards e consumo com props + estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import BarraDePesquisa from "./componentes/BarraDePesquisa";
 import Card from "./componentes/Card";
@@ -6,6 +7,15 @@ import Ordenacao from "./componentes/Ordenacao";
 import Sidebar from "./componentes/Sidebar";
 
 function App() {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => setDados(dados));
+  }, []);
+
   return (
     <div className="container">
       <Sidebar />
@@ -13,7 +23,24 @@ function App() {
         <BarraDePesquisa />
         <Filtro />
         <Ordenacao />
-        <Card />
+        <ul className="lista-cards">
+          {dados
+            ? dados.map((item, index) => (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    imagemUrl={item.imagem_capa}
+                    titulo={item.titulo}
+                    resumo={item.resumo}
+                    linhasDeCodigo={item.linhas_de_codigo}
+                    compartilhamentos={item.compartilhamentos}
+                    comentarios={item.comentarios}
+                    usuario={item.usuario}
+                  />
+                </li>
+              ))
+            : null}
+        </ul>
       </div>
     </div>
   );
--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -1,45 +1,48 @@
-import Capa from "./assets/capa.png";
+/* eslint-disable react/prop-types */
 import Code from "./assets/code.svg";
 import Chat from "./assets/chat.svg";
 import Share from "./assets/share.svg";
-import Icone from "./assets/icone.png";
 import "./styles.css";
 
-export default function Card() {
+export default function Card({
+  // eslint-disable-next-line no-unused-vars
+  id,
+  imagemUrl,
+  titulo,
+  resumo,
+  linhasDeCodigo,
+  compartilhamentos,
+  comentarios,
+  usuario,
+}) {
   return (
     <article className="card">
       <div className="card__imagem">
-        <img src={Capa} alt="Imagem do post" />
+        <img src={imagemUrl} alt="Imagem do post" />
       </div>
       <div className="card__conteudo">
         <div className="conteudo__texto">
-          <h3>Titulo do post</h3>
-          <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti quos
-            dolores et quas molestias excepturi sint.
-          </p>
+          <h3>{titulo}</h3>
+          <p>{resumo}</p>
         </div>
         <div className="conteudo__rodape">
-          <div>
-            <ul>
-              <li>
-                <img src={Code} alt="códigos" />
-                100
-              </li>
-              <li>
-                <img src={Share} alt="compartilhamentos" />
-                12
-              </li>
-              <li>
-                <img src={Chat} alt="comentários" />
-                12
-              </li>
-            </ul>
-          </div>
+          <ul>
+            <li>
+              <img src={Code} alt="códigos" />
+              {linhasDeCodigo}
+            </li>
+            <li>
+              <img src={Share} alt="compartilhamentos" />
+              {compartilhamentos}
+            </li>
+            <li>
+              <img src={Chat} alt="comentários" />
+              {comentarios}
+            </li>
+          </ul>
           <div className="rodape__usuario">
-            <img src={Icone} alt="Ícone do usuário" />
-            @Mateus
+            <img src={usuario.imagem} alt="Ícone do usuário" />
+            {usuario.nome}
           </div>
         </div>
       </div>
